da1c17f97a7f0b07291f1b09f1cb8555cecf84e8
Yori Verbist <yori.verbist1212@gmail.com>
2020-10-27 15:52:09 +0100

    Changes

--- script.R
+++ script.R
@@ -1,6 +1,8 @@
 library('ggplot2') # visualization
 library('dplyr') # data manipulation
 library('tidyr') # data manipulation
+library('stringr') # string manipulation
+
 
 train <- read.csv('./Input/train.csv', stringsAsFactors = F)
 
@@ -9,7 +11,7 @@ train <- train %>% mutate(
   Survived = factor(Survived),
   Pclass = factor(Pclass),
   Embarked = factor(Embarked),
-  Sex = factor(Sex)
+  Sex = factor(Sex),
 )
 
 test  <- read.csv('./Input/test.csv', stringsAsFactors = F)
@@ -21,53 +23,34 @@ test <- test %>% mutate(
   Sex = factor(Sex)
 )
 
-full  <- bind_rows(train, test) # bind training & test data
-
-# put all the different titles in a new variable
-full$Title <- gsub('(.*, )|(\\..*)', '', full$Name)
-
-# Titles with very low cell counts to be combined to "rare" level
-rare_title <- c('Dona', 'Lady', 'the Countess','Capt', 'Col', 'Don', 
-                'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer')
-
-# Also reassign mlle, ms, and mme accordingly
-full$Title[full$Title == 'Mlle']        <- 'Miss' 
-full$Title[full$Title == 'Ms']          <- 'Miss'
-full$Title[full$Title == 'Mme']         <- 'Mrs' 
-full$Title[full$Title %in% rare_title]  <- 'Rare Title'
+combine  <- bind_rows(train, test) # bind training & test data
 
 # Show title counts by sex again
-table(full$Sex, full$Title)
+table(train$Sex, train$Title)
 
 # Finally, grab surname from passenger name
-full$Surname <- sapply(full$Name,  
+train$Surname <- sapply(train$Name,  
                        function(x) strsplit(x, split = '[,.]')[[1]][1])
-
+ 
 # Create a family size variable including the passenger themselves
-full$Fsize <- full$SibSp + full$Parch + 1
+train$Fsize <- train$SibSp + train$Parch + 1
 
 # Put the family names in the table and add how big they are
-full$Family <- paste(full$Surname, full$Fsize, sep='_')
+train$Family <- paste(train$Surname, train$Fsize, sep='_')
 
 # check data
-str(full)
-summary(full)
+str(train)
+summary(train)
 
 aggr(combine, prop = FALSE, combined = TRUE, numbers = TRUE, sortVars = TRUE, sortCombs = TRUE)
-
-# Check how old the passengers were and divide them by gender
-ggplot(data = full, mapping = aes(x = Age, color = Sex)) +
-  geom_bar(binwidth = 1) +
-  theme_few()
 
 # Use ggplot2 to visualize the relationship between family size & survival
 # 0 = died
 # 1 = survived
-ggplot(full[1:891,], aes(x = Fsize, fill = Survived)) +
+ggplot(train, aes(x = Fsize, fill = Survived)) +
   geom_bar(stat='count', position='dodge') +
   scale_x_continuous(breaks=c(1:11)) +
-  labs(x = 'Family Size') +
-  theme_few()
+  labs(x = 'Family Size')
 
 ggplot(full, aes(x = Fare)) +
   geom_freqpoly()
@@ -79,8 +62,74 @@ train %>%
   scale_x_log10() +
   facet_wrap(~ Survived, ncol = 1)
 
+# Show survival rate filtered on where they boarded
 train %>%
   filter(Embarked %in% c("S","C","Q")) %>%
   ggplot() +
   geom_bar(aes(Embarked, fill = Pclass), position = "dodge") +
   facet_grid(~ Survived)
+
+# Show the how the density of people's survival rate odered by age and sex
+ggplot(train, aes(x=Age)) +
+  geom_density(aes(fill = Survived), alpha = 0.5) +
+  facet_wrap(~Sex)
+
+# Show how many family members each sex has on board
+train %>%
+  ggplot() +
+  geom_bar(aes(Parch, fill = Sex), position = "dodge") +
+  scale_y_log10() +
+  labs(x = '# of family members')
+
+train %>%
+  mutate(SibSp = factor(SibSp)) %>%
+  ggplot(aes(x=Age, color = SibSp)) +
+  geom_density(size = 1.5)
+
+combine <- mutate(combine,
+                  fclass = factor(log10(Fare+1) %/% 1),
+                  age_known = factor(!is.na(Age)),
+                  cabin_known = factor(!is.na(Cabin)),
+                  title_orig = factor(str_extract(Name, "[A-Z][a-z]*\\.")), # Re done by https://www.kaggle.com/nad136
+                  
+                  # Young people are people younger than 30
+                  young = factor( if_else(Age<=30, 1, 0, missing = 0) | (title_orig %in% c('Master.','Miss.','Mlle.')) ),
+                  child = Age<10,
+                  family = SibSp + Parch,
+                  alone = (SibSp == 0) & (Parch == 0),
+                  large_family = (SibSp > 2) | (Parch > 3),
+                  deck = if_else(is.na(Cabin),"U",str_sub(Cabin,1,1)),
+                  ttype = str_sub(Ticket,1,1),
+                  bad_ticket = ttype %in% c('1', '5', '6', '7', '8', 'A', 'F', 'W')
+)
+
+tgroup <- combine %>%
+  group_by(Ticket) %>%
+  summarise(ticket_group = n()) %>%
+  ungroup
+
+combine <- left_join(combine, tgroup, by = "Ticket") %>%
+  mutate(shared_ticket = ticket_group > 1)
+
+combine <- combine %>%
+  mutate(fare_eff = Fare/ticket_group,
+         title = fct_lump(title_orig, n=4),
+  )
+
+# Put the correct data back in the correct dataset
+train <- combine %>% filter(!is.na(Survived))
+test <- combine %>% filter(is.na(Survived))
+
+# Function to show some survival rates
+plot_bar_fill_grid <- function(barx, filly, gridx, gridy){
+  train %>%
+    ggplot(aes_string(barx, fill = filly)) +
+    geom_bar(position = "fill") +
+    facet_grid(reformulate(gridy,gridx))
+}
+
+# Show the survival rate of young people ordered by price class
+plot_bar_fill_grid("young", "Survived", "Sex", "Pclass")
+
+# Show the survival rate of the age of the person is known ordered by price class
+plot_bar_fill_grid("age_known", "Survived", "Sex", "Pclass")
